513d10f879d3f65e62646b60b0554d080843a5a8
Leona Juan <leonajuan@gmail.com>
2022-11-09 14:49:36 -0500

    added users for each review

--- app/serializers/rating_serializer.rb
+++ app/serializers/rating_serializer.rb
@@ -2,6 +2,7 @@ class RatingSerializer < ActiveModel::Serializer
   attributes :id, :user_id, :music_id, :rating, :comment, :show_star_ratings
 
   belongs_to :music
+  belongs_to :user
 
   def show_star_ratings
     "#{'★' * self.object.rating}"
--- client/src/components/Ratings.js
+++ client/src/components/Ratings.js
@@ -1,6 +1,7 @@
 function Ratings({ rating }) {
   return (
     <>
+      <p>@{rating.user.username} said:</p>
       <p>Rating: {rating.show_star_ratings}</p>
       <p>{rating.comment}</p>
     </>
