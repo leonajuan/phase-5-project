60aedc327ca39f2065160d818a0a82832c60b816
Leona Juan <leonajuan@gmail.com>
2022-11-09 14:39:01 -0500

    added stars

--- app/models/rating.rb
+++ app/models/rating.rb
@@ -6,4 +6,8 @@ class Rating < ApplicationRecord
   validates :rating, inclusion: 1..5
   validates :comment, length: { in: 3...250 }
   
+  def show_star_ratings
+    "#{'★' * self.rating}"
+  end
+
 end
